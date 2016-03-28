3557bec401caa1d92f7a419d6e37e792a0d5d387
RiiQQe <rickard.lindstedt@gmail.com>
2016-03-28 11:54:36 +0200

    added twitter bootstrap and navbar

--- app/app.js
+++ app/app.js
@@ -6,6 +6,50 @@
 		this.games = games;
 	});
 
+	app.controller('stryktipsCtrl', function(){
+		this.games = stryket;
+	});
+
+	app.controller('optionCtrl', function(){
+
+		this.tab = 1;
+		this.tabs = tabs;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		}
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		}
+
+	});
+
+	var tabs = [{name:'Choose yourself'}, { name:'Stryktipset' }, { name:'Europatipset' }, { name:'Topptipset'}];
+
+	var stryket = [{
+		score: '5-1',
+		teamhome: 'Arsenal FC',
+		teamaway: 'Liverpool FC',
+		teamawaywinner:false,
+		teamhomewinner:true,
+		goalscorer: [
+			'Ramsey',
+			'Giroud',
+			'Sanchez'
+		]
+	}, {
+		score: '5-5',
+		teamhome: 'Manchester City FC',
+		teamaway: 'Manchester United FC',
+		teamawaywinner:false,
+		teamhomewinner:false,
+		goalscorer: [
+			'Rooney',
+			'Aguero'
+		]
+	}];
+
+
 
 	var games = [{
 		score: '0-3',
@@ -22,7 +66,7 @@
 		score: '1-1',
 		teamhome: 'Manchester City FC',
 		teamaway: 'Manchester United FC',
-		teamawaywinner:true,
+		teamawaywinner:false,
 		teamhomewinner:false,
 		goalscorer: [
 			'Rooney',
