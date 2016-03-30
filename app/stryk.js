a3edcf72d7a7a51ec9a0184071c7a2908a6e9875
RiiQQe <rickard.lindstedt@gmail.com>
2016-03-30 19:32:14 +0200

    something that can be used for selecting

--- app/stryk.js
+++ app/stryk.js
@@ -5,7 +5,20 @@
 	app.directive('stryk', function(){
 		return {
 			restrict: 'E',
-			templateUrl: 'stryk.html'
+			templateUrl: 'stryk.html',
+			controller: function(){
+				//this.tab = 0;
+
+				this.selectTab = function(setTab){
+					this.tab = setTab;
+				}
+
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				}
+			},
+
+			controllerAs: 'stryketCtrl'
 		};
 	});
 
