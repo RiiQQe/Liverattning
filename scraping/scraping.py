15963c1d80e1315a89213f41612de46cb3986549
RiiQQe <rickard.lindstedt@gmail.com>
2016-03-30 12:55:46 +0200

    writing/reading to/from json instead

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,7 +1,8 @@
 from lxml import html
 import requests
 import re
-import csv
+#import csv
+import json
 
 class Game:
 	'common base class for all games'
@@ -63,23 +64,18 @@ while index < len(aList) and test :
 
 		index = index + 1
 		if aList[index].rfind('-') == -1 : 
-			temp = temp + aList[index]
 			hometeam = aList[index]
 			index = index + 1
-			temp = temp + aList[index]
 			awayteam = aList[index]
 		
 		else :
-			temp = temp + aList[index]
-			kalle = aList[index].split('-')
-			hometeam = kalle[0]
-			awayteam = kalle[1]
+			twoTeams = aList[index].split('-')
+			hometeam = twoTeams[0]
+			awayteam = twoTeams[1]
 
 		index = index + 1
-		temp = temp + aList[index]
 		score = aList[index]
 		index = index + 1
-		temp = temp + aList[index]
 		winner = aList[index]
 				
 		game = Game(gamenr, hometeam, awayteam, winner, score)
@@ -93,14 +89,21 @@ while index < len(aList) and test :
 
 index = 0
 
-f = open('stryk.csv', 'wt')
-try:
-    writer = csv.writer(f)
-    writer.writerow( ('gamenr', 'hometeam', 'awayteam', 'score', 'winner') )
-    for i in range(13):
-        writer.writerow( (newList[i].gamenr, newList[i].hometeam,  newList[i].awayteam,  newList[i].score,  newList[i].winner) )
-finally:
-    f.close()
+#f = open('stryk.csv', 'wt')
+#try:
+#    writer = csv.writer(f)
+#    writer.writerow( ('gamenr', 'hometeam', 'awayteam', 'score', 'winner') )
+#    for i in range(13):
+#        writer.writerow( (newList[i].gamenr, newList[i].hometeam,  newList[i].awayteam,  newList[i].score,  newList[i].winner) )
+#finally:
+#    f.close()
+
+with open("stryk.json", "w") as outfile:
+	jsonF = json.dump([ob.__dict__ for ob in newList], outfile)
+
+
+
+
 
 
 
