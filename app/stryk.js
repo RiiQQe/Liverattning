7a9b9e80514db745dbcfc732a8dbe7446f495846
RiiQQe <rickard.lindstedt@gmail.com>
2016-04-01 10:42:59 +0200

    added fillin

--- app/stryk.js
+++ app/stryk.js
@@ -2,34 +2,44 @@
 
 	var app = angular.module('stryket', []);
 
-	app.directive('strykDir', ['$http', function($http){
+	app.directive('strykDir', ['$http', '$filter', function($http, $filter){
 		return {
 			restrict: 'E',
 			templateUrl: 'stryk.html',
 			controller: function(){
 				//this.vals = [{'1':false, 'X':false, '2':false}];
-
+				//this.vals = [];
 				var stryk = this;
+				stryk.vals = [];
 
 				$http.get('scraping/stryk.json').success(function(data){
 					console.log(data)
 					stryk.games = data;
+					angular.forEach(stryk.games, function(d){
+						var obj = {'gamenr':d.gamenr, '1':false, 'X':false, '2':false};
+						stryk.vals.push(obj);
+					});
 				});
 				
-				console.log("here")
-
 				this.selectTab = function(setTab){
 					this.tab = setTab;
 				}
 
-				this.isSelected = function(checkTab){
-					return this.tab === checkTab;
+				this.isSelected = function(val, gamenr){
+
+					var found = $filter('filter')(this.vals, {gamenr:gamenr}, true);
+
+					return found[0][val];
 				}
 
-				this.setTab = function(val){
-					alert("wooorking");
-					//this.vals[val] = true;
+				this.setTab = function(val, gamenr){
+					var found = $filter('filter')(this.vals, {gamenr:gamenr}, true);
+
+					if(found[0][val]) found[0][val] = false;
+					else found[0][val] = true;
 				}
+
+
 
 			},
 			controllerAs: 'stryketCtrl'
